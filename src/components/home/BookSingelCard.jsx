e7546e52f899e4924fb2572761a8c5f077f9d09f
ddmuddatsir <dedemudasir@gmail.com>
2023-10-26 21:29:14 +0700

    frontend crud is done

--- src/components/home/BookSingelCard.jsx
+++ src/components/home/BookSingelCard.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
 import { PiBookOpenTextLight } from "react-icons/pi";
-import { BiUserCircle } from "react-icons/bi";
+import { BiShow, BiUserCircle } from "react-icons/bi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import PropTypes from "prop-types";
+import { useState } from "react";
+import BookModal from "./BookModal";
 
-const BookSingelCard = ({ book }) => {
+const BookSingleCard = ({ book }) => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
-    <div
-      key={book._id}
-      className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
-    >
+    <div className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
       <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
         {book.publishYear}
       </h2>
@@ -25,21 +26,28 @@ const BookSingelCard = ({ book }) => {
         <h2 className="my-1">{book.author}</h2>
       </div>
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+        <BiShow
+          className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+          onClick={() => setShowModal(true)}
+        />
         <Link to={`/books/details/${book._id}`}>
           <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
         </Link>
-        <Link to={`/books/details/${book._id}`}>
+        <Link to={`/books/edit/${book._id}`}>
           <AiOutlineEdit className="text-2xl text-yellow-600 hover:text-black" />
         </Link>
-        <Link to={`/books/details/${book._id}`}>
+        <Link to={`/books/delete/${book._id}`}>
           <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
         </Link>
       </div>
+      {showModal && (
+        <BookModal book={book} onClose={() => setShowModal(false)} />
+      )}
     </div>
   );
 };
 
-BookSingelCard.propTypes = {
+BookSingleCard.propTypes = {
   book: PropTypes.shape({
     _id: PropTypes.string.isRequired,
     publishYear: PropTypes.number.isRequired,
@@ -48,4 +56,4 @@ BookSingelCard.propTypes = {
   }).isRequired,
 };
 
-export default BookSingelCard;
+export default BookSingleCard;
